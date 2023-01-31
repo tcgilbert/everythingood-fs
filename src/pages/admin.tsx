10c73bfd47e9624c25c62e453d6c30a6e6689b3d
Thomas Gilbert <tcgilbertt@gmail.com>
2023-01-31 18:01:18 -0500

    pages for admin route

---
+++ src/pages/admin.tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from "next";
+
+const Admin: NextPage = () => {
+  return (
+    <>
+      <div className="">Admin Should Only See this</div>
+    </>
+  );
+};
+
+export default Admin;
---
+++ src/pages/login.tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from "next";
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <div className="">Login Should Only See this</div>
+    </>
+  );
+};
+
+export default Login;
