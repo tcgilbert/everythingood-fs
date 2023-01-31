03a8e566a64122464bcd5b6b69b94cf34a626df2
Thomas Gilbert <tcgilbertt@gmail.com>
2023-01-30 20:41:15 -0500

    backend function to fetch static data

--- src/pages/les.tsx
+++ src/pages/les.tsx
@@ -1,42 +1,22 @@
 import { type NextPage } from "next";
 
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
-import superjson from "superjson";
-import { appRouter } from "../server/api/root";
-import { createInnerTRPCContext } from "../server/api/trpc";
-import { api } from "../utils/api";
+import { getStaticGuideData } from "../server/queries/guideData";
 
 export const getStaticProps = async () => {
-  const ssg = createProxySSGHelpers({
-    router: appRouter,
-    ctx: createInnerTRPCContext({}),
-    transformer: superjson,
-  });
-
-  await ssg.guide.getAll.prefetch({ guide: "les" });
+  const data = await getStaticGuideData("les");
 
   return {
     props: {
-      trpcState: ssg.dehydrate(),
+      data,
     },
   };
 };
 
 const Les: NextPage = (props) => {
-  const resData = api.guide.getAll.useQuery({ guide: "les" });
-
-  const showData = resData.data?.restaurants.map((restaurant) => {
-    return (
-      <div key={restaurant.id}>
-        <h1>{restaurant.name}</h1>
-      </div>
-    );
-  });
-
   return (
     <>
       <div className="">This tha page</div>
-      {showData}
+      {/* {showData} */}
     </>
   );
 };
