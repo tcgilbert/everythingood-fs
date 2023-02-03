422a8cc76f98bd7144017f93612063d8d255fa16
Thomas Gilbert <tcgilbertt@gmail.com>
2023-02-02 19:50:10 -0500

    soho noho page

--- src/pages/soho-noho.tsx
+++ src/pages/soho-noho.tsx
@@ -1,11 +1,14 @@
-import { type NextPage } from "next";
+import { InferGetStaticPropsType, type NextPage } from "next";
 import Image from "next/image";
-import LowerEastSideMap from "../public/les-map.jpg";
+import { useState } from "react";
+import { GuideContent } from "../components/GuideContent";
+import { GuideNav } from "../components/GuideNav";
+import SohoNohoMap from "../public/sohonoho-map.jpg";
 
 import { getStaticGuideData } from "../server/queries/guideData";
 
 export const getStaticProps = async () => {
-  const data = await getStaticGuideData("les");
+  const data = await getStaticGuideData("soho-noho");
 
   return {
     props: {
@@ -14,7 +17,8 @@ export const getStaticProps = async () => {
   };
 };
 
-const SohoNoho: NextPage = (props) => {
+const SohoNoho = ({ data }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const [displayed, setDisplayed] = useState("restaurants");
   return (
     <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
       {/* intro */}
@@ -29,61 +33,74 @@ const SohoNoho: NextPage = (props) => {
         <div className="px-4 py-5 sm:p-6">
           <p className="text-center font-eb text-lg">
             <span className="font-eb text-red-500">Don’t worry. </span>
-            This is not a screenplay about a Gen Z creative or a pitch about a
-            Dimes Square reality TV show or a press release about a luxury brand
-            making $900 bags from old takeout menus or something useless like
-            that.{" "}
+            This is not a list of the best spots to get street-styled or a
+            manual for bribing the Balthazar maître d’ or best practices for
+            securing NikeLab releases or a map of where the Jenners and Hadids
+            have apartments.{" "}
             <span className="font-eb text-red-500">
               It’s (hopefully) not eye roll material.
             </span>
           </p>
           <div className="py-3" />
           <p className="text-center font-eb text-lg">
-            This is quite literally a guide to everything good in the LES and
-            Two Bridges, from art galleries to bakeries to coffee shops to
-            clothing boutiques to bars and restaurants.{" "}
             <span className="font-eb text-red-500">
               Everything Good is a very thorough guide to neighborhoods in NYC.{" "}
             </span>
-            It is a complete, single-source docket meant to give you 360 degree
-            recommendations that span the entirety of a neighborhood.
+            They are complete, single-source dockets meant to give you 360
+            degree recommendations that span the entirety of a neighborhood, and
+            this is quite literally a guide to everything good in the
+            fashionable, see-and-be-seen, sometimes crowded, oftentimes
+            insufferable neighborhoods of SoHo and NoHo. It Includes everything
+            from art galleries to bakeries to coffee shops to clothing boutiques
+            to bars and restaurants. And while there’s a lot of noise in these
+            two neighborhoods, they are also home to some of the best food and
+            shopping in all of NYC.
           </p>
           <div className="py-3" />
           <p className="text-center font-eb text-lg">
-            This is the first installment of Everything Good.
+            Anyway, this is the second installment of Everything Good.
             <span className="font-eb text-red-500">
-              {" "}
-              There will be many more.{" "}
+              You can find the first guide to the LES right here. There will be
+              many more.
             </span>
-            I’m the only person writing and gathering these recommendations. If
-            you do not see a spot (well-known or not) on this list, it might be
-            because I don’t know it. But most likely, it’s because I don’t
+            I’m the only person writing and gathering these recommendations, so
+            if you do not see a spot (well-known or not) on this list, it might
+            be because I don’t know it. But most likely, it’s because I don’t
             really like it.
           </p>
           <div className="py-3" />
           <p className="text-center font-eb text-lg">
             As you may have noticed, this guide is not a design object. It’s a
             Google Doc. I could have tried harder to make this look cool, but I
-            didn’t. TBH, my Adobe Suite subscription expired. There are some
-            iPhone photos to give added flavor to these recommendations, which
-            are broken down by categories and presented with information I think
-            is important to know about each spot. This is a long list. More is
-            (allegedly) more.
+            didn’t. The point is just to give you recommendations as directly as
+            possible. There are some iPhone photos to give added flavor to these
+            recommendations, which are broken down by categories and presented
+            with information I think is important to know about each spot. This
+            is a long list.
+            <span className="font-eb text-red-500">
+              More is (allegedly) more.
+            </span>
           </p>
           <div className="py-3" />
           <p className="text-center font-eb text-lg">
-            I thought about charging a small fee for this information, but
-            instead, for the first installment, I’d love it if you could make a
-            donation to the Henry Street Settlement, an amazing charity in the
-            LES that provides childcare, cultural programming, housing, art
-            classes, health care, and social services for LES residents in need.
-            If you’re using this list, please donate!
+            For this installment of Everything Good, I am keeping access free,
+            but I’d love it if you could make a donation to{" "}
+            <a
+              className="text-red-600"
+              href="https://give.glwd.org/give/244921/?&_ga=2.82856336.720855393.1659365992-1819495194.1658185824&_gac=1.152424907.1659365992.Cj0KCQjw852XBhC6ARIsAJsFPN2ROn8IwnasS4uPZ7g5p2G1i2Dm1jOOEHF6rAXr9556xlUoF4Cy8aMaAjprEALw_wcB#!/donation/checkout?c_src=WS&c_src2=BU"
+            >
+              God’s Love We Deliver
+            </a>
+            , an amazing secular charity in SoHo that provides nutritious meals
+            for New Yorkers that are too sick or elderly to cook for themselves.
+            They’ve done fantastic work in NYC since 1985 and have been at home
+            in SoHo since 2015. If you’re using this list, please donate!
           </p>
           <div className="py-3" />
           <p className="text-center font-eb text-lg">
             I hope you find this list useful. It might not be. Either way, share
             it with your friends, donate, support these businesses, and have an
-            excellent day in the LES. Cheers!
+            excellent day in SoHo and NoHo. Cheers!
           </p>
           <div className="py-3" />
           <p className="text-center font-eb text-lg">
@@ -95,21 +112,17 @@ const SohoNoho: NextPage = (props) => {
               — Alex Delany
             </a>
           </p>
-        </div>
-      </div>
-      <div className="my-5" />
-      <div className="overflow-hidden rounded-lg bg-gray-100 shadow">
-        <div className="px-4 py-5 sm:p-6">
-          {" "}
+          <div className="py-3" />
           <div className="mx-auto max-w-3xl">
             <Image
-              src={LowerEastSideMap}
+              src={SohoNohoMap}
               priority={true}
-              alt="Map of the Lower East Side"
+              alt="Map of the Soho and Noho"
               className="shadow-lg"
             />
           </div>
           <div className="py-3" />
+
           <p className="text-center font-eb text-lg">
             OK, OK, OK. First off, when I say the LES and Two Bridges I mean the
             space outlined in black above. East of The Bowery. South of Houston.
@@ -145,6 +158,9 @@ const SohoNoho: NextPage = (props) => {
           </p>
         </div>
       </div>
+      <GuideNav displayed={displayed} setDisplayed={setDisplayed} />
+      <div className="my-3" />
+      <GuideContent displayed={displayed} data={data} />
     </div>
   );
 };
