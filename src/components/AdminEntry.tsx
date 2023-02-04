47f6fd65072d6bc4e78d3d43d2261195ac432f1b
Thomas Gilbert <tcgilbertt@gmail.com>
2023-02-03 20:23:57 -0500

    modal styling

--- src/components/AdminEntry.tsx
+++ src/components/AdminEntry.tsx
@@ -33,123 +33,220 @@ export const AdminEntry = (props: AdminEntryProps) => {
     switch (key) {
       case "name":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Name:</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Name:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "description":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Description:</label>
-            <textarea value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Description:
+            </label>
+            <textarea
+              rows={4}
+              name="comment"
+              id="comment"
+              className="block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              value={value}
+            />
           </div>
         );
       case "price":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Price:</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Price:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "idealGroupNumber":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Ideal Group Number</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Ideal Group Number
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "bestThingOnTheMenu":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Best Thing on the Menu:</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Best Thing on the Menu:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "idealMeal":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Ideal Meal</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Ideal Meal:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "alsoGreat":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Also Great</label>
-            <textarea value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Also Great:
+            </label>
+            <textarea
+              rows={4}
+              name="comment"
+              id="comment"
+              className="block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              value={value}
+            />
           </div>
         );
       case "note":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Note:</label>
-            <textarea value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Note:
+            </label>
+            <textarea
+              rows={4}
+              name="comment"
+              id="comment"
+              className="block w-full rounded-md border border-gray-300 p-2 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+              value={value}
+            />
           </div>
         );
       case "link":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Link</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Link:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "drinkOrder":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Drink Order</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Drink Order:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "drinkSpecialty":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Drink Specialty:</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Drink Specialty:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "food":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Food:</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Food:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "coffeeBeans":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Coffee Beans:</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Coffee Beans:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "roomToHang":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Room to Hang:</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Room to Hang:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "specialty":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Specialty:</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Specialty:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       case "bakedGoods":
         return (
-          <div key={key} className="flex flex-col">
-            <label>Baked Goods:</label>
-            <input type="text" value={value} />
+          <div key={key} className="flex flex-col py-1">
+            <label className="block text-sm font-medium text-gray-700">
+              Baked Goods:
+            </label>
+            <input
+              className="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              type="text"
+              value={value}
+            />
           </div>
         );
       default:
-        return (
-          <div key={key}>
-            <div>{key}</div>
-            <div>{value}</div>
-          </div>
-        );
+        return null;
     }
   }
 
@@ -201,8 +298,11 @@ export const AdminEntry = (props: AdminEntryProps) => {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-gray-400 px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                <Dialog.Panel className="relative w-3/4 transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all">
                   {props.data && RenderObject(props.data)}
+                  <button className=" my-3 rounded-md border border-transparent bg-red-600 py-2 px-4 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2">
+                    Save
+                  </button>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
