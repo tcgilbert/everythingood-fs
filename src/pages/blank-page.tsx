272637b6ea2b37e265611e56ce94526a3356fa91
Thomas Gilbert <tcgilbertt@gmail.com>
2023-02-05 18:39:08 -0500

    cleanup

--- src/pages/blank-page.tsx
+++ src/pages/blank-page.tsx
@@ -1,5 +1,3 @@
-import { MobileRecsNav } from "../components/MobileRecsNav";
-
 const BlankPage = () => {
   return (
     <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
@@ -9,7 +7,6 @@ const BlankPage = () => {
         <div className="h-96 w-full bg-pink-400">2</div>
         <div className="h-96 w-full bg-green-500">4</div>
       </div>
-      <MobileRecsNav />
       <div className="h-96 w-full bg-white">4</div>
       <div className="h-96 w-full bg-white">4</div>
       <div className="h-96 w-full bg-white">4</div>
